a1fa26f225935d1215d5404e16c67245ad24ad8f
Mr Ruba <zacharyjcanderson@gmail.com>
2019-09-07 00:58:03 -0700

    Added firebase into the counter component to recored score.

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react"
 
+
+
+import Firebase from 'firebase';
+import config from './../../controllers/firebase-admin/config.js';
+
+
 class Counter extends Component {
   constructor(props){
     super(props);
     
+    Firebase.initializeApp(config);
+
     this.state = {
       constructive_count: 0,
       destructive_count: 0
@@ -12,6 +20,18 @@ class Counter extends Component {
     console.log("Counter Created");
   }
   
+  //Reads data after a render...
+  componentDidMount(){
+    this.readData();
+  }
+  
+  //Writes data after state change (and before render!)...
+  componentDidUpdate(prop, state) {
+    if(state !== this.state){
+      this.writeData(); 
+    }
+  };
+
   construct = () => {
     console.log("Construct Clicked!");
     this.setState({
@@ -26,6 +46,17 @@ class Counter extends Component {
       destructive_count: this.state.destructive_count + 1
     });
     console.log(this.state.destructive_count);
+  }
+  
+  writeData = () => {
+    Firebase.database().ref('/').set(this.state);
+    console.log("Saving...");
+  }
+
+  readData = () => {
+    let ref = Firebase.database().ref('/');
+    ref.on('value', database => {this.setState(database.val())});
+    console.log("Reading....");
   }
 
   render() {
